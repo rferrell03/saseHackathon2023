37976f24431b983cd7093ae9cc303e4204e151f3
Matthew Posey <mparkerposey@outlook.com>
2023-09-30 19:34:53 -0500

    summin

--- scripts.js
+++ scripts.js
@@ -4,58 +4,65 @@ var APIKey = "4d71d022dd444af3815791c9ea3f4d44";
 //REMOVE
 var numOfRecipes = 10;
 var bulkRecipeString = "";
-
+var bestRecipeId = "";
 var RecipeNames = [];
 var RecipePrepTimes = [];
 var RecipeCookTimes = [];
 var RecipeServings = [];
 var RecipeComplexity = [];
 var RecipeSteps = [];
+var bestRecipeId;
 
-
-var meals = fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=" + `${numOfRecipes}` + "&ranking=1&ignorePantry=true&apiKey=" + APIKey).then(res => res.json()).then(res => displayMeals(res));
-function displayMeals(meal)
+function getRecipes()
 {
-    
-    
-    var IDString = "";
-   
-    for (let i = 0; i < numOfRecipes; i++)
+    var meals = fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=" + `${numOfRecipes}` + "&ranking=1&ignorePantry=true&apiKey=" + APIKey).then(res => res.json()).then(res => displayMeals(res));
+    function displayMeals(meal)
     {
-        if (IDString.length > 1)
-        {
-            IDString +=  "," + meal[i].id;
-            RecipeNames[i] = meal[i].title;
-           
-        }
-        else
-        {
-            IDString += meal[i].id;
-            RecipeNames[i] = meal[i].title;
-        }
         
+        
+        var IDString = "";
+    
+        for (let i = 0; i < numOfRecipes; i++)
+        {
+            if (IDString.length > 1)
+            {
+                IDString +=  "," + meal[i].id;
+                RecipeNames[i] = meal[i].title;
+            
+            }
+            else
+            {
+                IDString += meal[i].id;
+                RecipeNames[i] = meal[i].title;
+            }
+            
+        }
+        console.log(RecipeNames);
+        bestRecipeId = meal[0].id;
+        console.log(bestRecipeId);
+        getRecipeDetails();
+        
+        
+        // recipe[0].key1 = {key2:"hello"};
+        // console.log(recipe[0].key1.key2);
     }
-    console.log(RecipeNames);
-    bulkRecipeString = meal[0].id;
-    console.log(IDString);
-    
-    
-    // recipe[0].key1 = {key2:"hello"};
-    // console.log(recipe[0].key1.key2);
 }
 
-var recipes = fetch("https://api.spoonacular.com/recipes/"+`${bulkRecipeString}`+"/analyzedInstructions?apiKey=" + APIKey).then(res => res.json()).then(res => displayRecipes(res));
-function displayRecipes(recipe)
+function getRecipeDetails()
 {
-    for(let i =0;i<numOfRecipes;i++){
-        console.log(recipe[0].steps[i].step);
+    var recipes = fetch("https://api.spoonacular.com/recipes/" + bestRecipeId + "/analyzedInstructions?apiKey=" + APIKey).then(res => res.json()).then(res => displayRecipes(res));
+    function displayRecipes(recipe)
+    {
+        // for(let i =0;i<numOfRecipes;i++){
+        //     console.log(recipe);
+        // }
+        console.log(recipe);
+        console.log(bestRecipeId);
     }
-    console.log(recipe);
 }
-// function addRecipe() {
-//     var newElement = document.createElement("div");
-//     newElement.textContent = meals;
-//     recipeContainer.append(newElement);
-// }
+
+function addRecipe() {
+    getRecipes();
+}
 
 
